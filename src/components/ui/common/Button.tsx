9d626811f82a75cbd95c8ae0ee2daa97d836e63f
Hailey Kim <haileydeveloper@gmail.com>
2024-07-12 17:08:34 +0900

    feat: add className, onClick props to Buttom component

--- src/components/ui/common/Button.tsx
+++ src/components/ui/common/Button.tsx
@@ -3,15 +3,24 @@ import { cn } from "@/lib/utils";
 interface Props {
   content: string;
   primary?: boolean;
+  className?: string;
+  onClick?: () => void;
 }
 
-export default function Button({ content, primary }: Props) {
+export default function Button({
+  content,
+  primary,
+  className,
+  onClick,
+}: Props) {
   return (
     <button
       className={cn(
         "w-full py-[15px] rounded-md border border-gray-900",
-        primary ? "bg-gray-900" : "bg-gray-50"
+        primary ? "bg-gray-900" : "bg-gray-50",
+        className
       )}
+      onClick={onClick}
     >
       <span
         className={cn(
