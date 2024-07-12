c0bb9bb361ea365d9a563a4387c4b41d0916f20b
Hailey Kim <haileydeveloper@gmail.com>
2024-07-12 19:53:40 +0900

    chore: add Input, IdentificationForm to stories and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Button from "@/components/common/Button";
+import IdentificationForm from "@/components/login/organisms/IdentificationForm";
 import LoginForm from "@/components/login/organisms/LoginForm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between gap-6 p-24">
       <Button content="button" primary />
       <LoginForm />
+      <IdentificationForm />
     </main>
   );
 }
