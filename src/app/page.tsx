80a290928cb5775a25563ec0a67f923256e0ef12
Hailey Kim <haileydeveloper@gmail.com>
2024-07-12 20:49:23 +0900

    chore: add ResetPasswordForm to stories and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Button from "@/components/common/Button";
+import Input from "@/components/common/Input";
 import IdentificationForm from "@/components/login/organisms/IdentificationForm";
 import LoginForm from "@/components/login/organisms/LoginForm";
+import ResetPasswordForm from "@/components/login/organisms/ResetPasswordForm";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-6 p-24">
-      <Button content="button" primary />
+      <Button content="버튼" primary />
+      <Input name="input" type="text" placeholder="텍스트를 작성해보세요!" />
       <LoginForm />
       <IdentificationForm />
+      <ResetPasswordForm />
     </main>
   );
 }
