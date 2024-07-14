466631dd649225dda43a8c19afed4a6988f8a07c
Hailey Kim <haileydeveloper@gmail.com>
2024-07-14 15:41:01 +0900

    feat: update Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import ProfileDropdown from "@/components/header/atoms/ProfileDropdown";
 import Logo from "@/assets/logos/logo.svg";
+import ComoLogo from "@/assets/logos/como_logo.svg";
 
 interface Props {
   isDashboard?: boolean;
@@ -12,7 +13,7 @@ interface Props {
 
 export default function Header(props: Props) {
   return (
-    <header className="fixed top-0 inset-x-0 flex items-center justify-center h-24 border-b border-gray-300 bg-gray-0">
+    <header className="fixed top-0 inset-x-0 z-20 flex items-center justify-center h-24 border-b border-gray-300 bg-gray-0">
       <div
         className={cn(
           "flex items-center justify-between h-9 px-8",
@@ -21,22 +22,26 @@ export default function Header(props: Props) {
       >
         <Link href={"/"}>
           <h1 className="flex gap-3 font-bold text-gray-900">
-            <Image src={Logo} alt="OMO" width={36} height={36} />
+            {props.isDashboard ? (
+              <Image src={Logo} alt="OMO" width={36} height={36} />
+            ) : (
+              <Image src={ComoLogo} alt="c'omo for business" width={186} />
+            )}
             {props.title && props.title}
           </h1>
         </Link>
         <div className="flex items-center gap-[60px]">
           <Link href={"/"}>
-            <span className="h3 font-semibold text-gray-900">{"공지사항"}</span>
+            <span className="h4 font-normal text-gray-700">{"공지사항"}</span>
           </Link>
           <Link href={"/"}>
-            <span className="h3 font-semibold text-gray-900">{"고객센터"}</span>
+            <span className="h4 font-normal text-gray-700">{"고객센터"}</span>
           </Link>
           {props.isLoggedIn ? (
             <ProfileDropdown />
           ) : (
             <Link href={"/login"}>
-              <span className="h3 font-semibold text-gray-900">{"로그인"}</span>
+              <span className="h4 font-normal text-gray-700">{"로그인"}</span>
             </Link>
           )}
         </div>
