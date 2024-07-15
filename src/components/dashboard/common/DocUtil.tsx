9c6ebf6b5d5b7d4ca8f88cdcc58d9d9414daabd2
Hailey Kim <haileydeveloper@gmail.com>
2024-07-15 15:59:42 +0900

    feat: create DocUtilButtons

---
+++ src/components/dashboard/common/DocUtil.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import PrintIcon from "@/assets/icons/print.svg";
+import DocIcon from "@/assets/icons/document.svg";
+
+export default function DocUtilButtons() {
+  return (
+    <div className="flex gap-3">
+      <button className="p-1 rounded bg-gray-900">
+        <Image src={PrintIcon} alt="출력" width={24} height={24} />
+      </button>
+      <button className="p-1 rounded bg-point-green">
+        <Image src={DocIcon} alt="저장" width={24} height={24} />
+      </button>
+    </div>
+  );
+}
