7fde7c6a857cbf9c99cc9d53c78c64c96f89c9f5
Hailey Kim <haileydeveloper@gmail.com>
2024-07-15 11:03:29 +0900

    chore: create new route /support

---
+++ src/app/support/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header/Header";
+
+export default function SupportLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header isLoggedIn title="동호회 관리센터" />
+      <main className="flex justify-center mt-24 py-[150px]">{children}</main>
+    </>
+  );
+}
---
+++ src/app/support/page.tsx
@@ -0,0 +1,5 @@
+import LoginForm from "@/components/login/organisms/LoginForm";
+
+export default function SupportPage() {
+  return <LoginForm />;
+}
