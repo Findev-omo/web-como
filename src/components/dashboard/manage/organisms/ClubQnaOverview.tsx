6f77d5aaaa15a37a519518717fe7952f0a66afc9
Hailey Kim <haileydeveloper@gmail.com>
2024-07-16 09:22:57 +0900

    feat: separate Overview and make space for question detail page

---
+++ src/components/dashboard/manage/organisms/ClubQnaOverview.tsx
@@ -0,0 +1,23 @@
+export default function ClubQnaOverview() {
+  return (
+    <div className="space-y-4 p-[38px] rounded-xl bg-gray-0">
+      <h2 className="h1 font-bold text-gray-900">{"주요 알림"}</h2>
+      <div className="flex">
+        <div className="flex items-center gap-4">
+          <span className="h3 font-bold text-gray-600">{"오늘의 Q&A"}</span>
+          <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
+        </div>
+        <span className="mx-10 border-l border-gray-400" />
+        <div className="flex items-center gap-4">
+          <span className="h3 font-bold text-gray-600">{"답변 대기"}</span>
+          <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
+        </div>
+        <span className="mx-10 border-l border-gray-400" />
+        <div className="flex items-center gap-4">
+          <span className="h3 font-bold text-gray-600">{"답변 완료"}</span>
+          <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/dashboard/manage/templates/ClubQna.tsx
+++ src/components/dashboard/manage/templates/ClubQna.tsx
@@ -1,28 +1,23 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import ClubQnaOverview from "@/components/dashboard/manage/organisms/ClubQnaOverview";
 import ClubQnaList from "@/components/dashboard/manage/organisms/ClubQnaList";
 
 export default function ClubQnaTab() {
+  const { back } = useRouter();
+  const question = useSearchParams().get("question");
+
   return (
     <>
-      <div className="space-y-4 p-[38px] rounded-xl bg-gray-0">
-        <h2 className="h1 font-bold text-gray-900">{"주요 알림"}</h2>
-        <div className="flex">
-          <div className="flex items-center gap-4">
-            <span className="h3 font-bold text-gray-600">{"오늘의 Q&A"}</span>
-            <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
-          </div>
-          <span className="mx-10 border-l border-gray-400" />
-          <div className="flex items-center gap-4">
-            <span className="h3 font-bold text-gray-600">{"답변 대기"}</span>
-            <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
-          </div>
-          <span className="mx-10 border-l border-gray-400" />
-          <div className="flex items-center gap-4">
-            <span className="h3 font-bold text-gray-600">{"답변 완료"}</span>
-            <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
-          </div>
-        </div>
-      </div>
-      <ClubQnaList />
+      {question ? (
+        <></>
+      ) : (
+        <>
+          <ClubQnaOverview />
+          <ClubQnaList />
+        </>
+      )}
     </>
   );
 }
