dd9236072b597e2a97557b6ad9c30bb52ca77328
Hailey Kim <haileydeveloper@gmail.com>
2024-07-16 11:24:35 +0900

    feat: update DocUtil (make each button into separate components)

--- src/components/dashboard/common/DocUtil.tsx
+++ src/components/dashboard/common/DocUtil.tsx
@@ -2,15 +2,27 @@ import Image from "next/image";
 import PrintIcon from "@/assets/icons/utilButton/print.svg";
 import DocIcon from "@/assets/icons/utilButton/document.svg";
 
+export const PrintButton = () => {
+  return (
+    <button className="p-1 rounded bg-gray-900">
+      <Image src={PrintIcon} alt="출력" width={24} height={24} />
+    </button>
+  );
+};
+
+export const SaveButton = () => {
+  return (
+    <button className="p-1 rounded bg-point-green">
+      <Image src={DocIcon} alt="저장" width={24} height={24} />
+    </button>
+  );
+};
+
 export default function DocUtilButtons() {
   return (
     <div className="flex gap-3">
-      <button className="p-1 rounded bg-gray-900">
-        <Image src={PrintIcon} alt="출력" width={24} height={24} />
-      </button>
-      <button className="p-1 rounded bg-point-green">
-        <Image src={DocIcon} alt="저장" width={24} height={24} />
-      </button>
+      <PrintButton />
+      <SaveButton />
     </div>
   );
 }
