7246fea59e56d2a290bf12e9ceed4e5f717c6d4a
Hailey Kim <haileydeveloper@gmail.com>
2024-07-18 06:04:36 +0900

    feat: add expand collapse feature to ItemDetail

--- src/components/dashboard/reservation/organisms/ItemDetail.tsx
+++ src/components/dashboard/reservation/organisms/ItemDetail.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 import CopyButton from "@/components/dashboard/common/CopyButton";
 import ChevronDownIcon from "@/assets/icons/chevron_down.svg";
 
 export default function ItemDetail() {
+  const [isInfoExpanded, setIsInfoExpanded] = useState<boolean>(false);
+
   const image = null;
   const address = "서울 마포구 양화로11길 54";
 
@@ -10,14 +16,32 @@ export default function ItemDetail() {
     <div className="flex flex-col gap-8 p-8 rounded-xl bg-gray-0">
       <div className="flex flex-col gap-3">
         <h4 className="font-bold text-gray-900">{"컨텐츠 상세 소개"}</h4>
-        <div className="relative mb-5">
-          <div className="flex-1 relative w-full min-h-60 h-full object-contain bg-gray-300">
-            {image && <Image src={image} alt="상세 이미지" fill sizes="60vw" />}
+        <div className={cn("relative", isInfoExpanded ? "" : "h-[280px]")}>
+          <div className={isInfoExpanded ? "" : "h-[260px] overflow-y-clip"}>
+            <div className="relative w-full h-[800px] object-contain bg-gray-300">
+              {image && (
+                <Image src={image} alt="상세 이미지" fill sizes="60vw" />
+              )}
+            </div>
           </div>
-          <div className="absolute bottom-0 w-full h-[130px] bg-gradient-to-b from-gray-50/0 to-gray-50" />
-          <button className="relative bottom-7 flex items-center justify-center gap-[3px] w-8/12 mx-auto py-3 rounded-md border border-gray-300 h4 font-semibold text-gray-700 bg-gray-50">
-            {"상세정보 더보기"}
-            <Image src={ChevronDownIcon} alt="▼" width={24} height={24} />
+          {!isInfoExpanded && (
+            <div className="absolute bottom-5 w-full h-[130px] bg-gradient-to-b from-gray-50/0 to-gray-50" />
+          )}
+          <button
+            className="relative bottom-7 flex items-center justify-center gap-[3px] w-8/12 mx-auto py-3 rounded-md border border-gray-300 h4 font-semibold text-gray-700 bg-gray-50"
+            onClick={() => setIsInfoExpanded((prev) => !prev)}
+          >
+            {isInfoExpanded ? "상세정보 접기" : "상세정보 더보기"}
+            <Image
+              src={ChevronDownIcon}
+              alt="▼"
+              width={24}
+              height={24}
+              className={cn(
+                "transition-all duration-300",
+                isInfoExpanded ? "rotate-180" : ""
+              )}
+            />
           </button>
         </div>
       </div>
