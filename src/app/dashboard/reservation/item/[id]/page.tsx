1282910f761f06f9c32686b014346a7275afdc30
Hailey Kim <haileydeveloper@gmail.com>
2024-07-17 22:03:17 +0900

    feat: update ItemDetail page and globals.css

--- src/app/dashboard/reservation/item/[id]/page.tsx
+++ src/app/dashboard/reservation/item/[id]/page.tsx
@@ -1,5 +1,6 @@
 import BackButton from "@/components/dashboard/common/BackButton";
 import InquiryShortcut from "@/components/dashboard/reservation/molecules/InquiryShortcut";
+import ItemDetailOverview from "@/components/dashboard/reservation/molecules/ItemOverview";
 import ItemDetail from "@/components/dashboard/reservation/organisms/ItemDetail";
 import ReservationPanel from "@/components/dashboard/reservation/organisms/ReservationPanel";
 
@@ -7,12 +8,12 @@ export default function ItemDetailPage() {
   return (
     <>
       <BackButton />
-      <div className="flex gap-3">
+      <ItemDetailOverview />
+      <div className="flex items-start gap-3">
         <div className="flex-1 flex flex-col gap-3">
           <ItemDetail />
           <InquiryShortcut />
         </div>
-        {/* <div className="w-[490px]" /> */}
         <ReservationPanel />
       </div>
     </>
