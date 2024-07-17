159527961fb329fc71fab7fb357a0eeb5a484521
Hailey Kim <haileydeveloper@gmail.com>
2024-07-18 08:40:19 +0900

    feat: update QnaOverview (new style)

--- src/components/dashboard/manage/organisms/ClubQnaOverview.tsx
+++ src/components/dashboard/manage/organisms/ClubQnaOverview.tsx
@@ -1,21 +1,16 @@
 export default function ClubQnaOverview() {
   return (
-    <div className="space-y-4 p-[38px] rounded-xl bg-gray-0 select-none">
-      <h2 className="h1 font-bold text-gray-900">{"주요 알림"}</h2>
-      <div className="flex">
-        <div className="flex items-center gap-4">
-          <span className="h3 font-bold text-gray-600">{"오늘의 Q&A"}</span>
+    <div className="space-y-6 p-8 rounded-xl bg-gray-800 select-none">
+      <h2 className="h1 font-bold text-gray-0">{"주요 알림"}</h2>
+      <div className="flex py-3 px-2">
+        <div className="flex-1 flex flex-col gap-4">
+          <span className="h4 font-medium text-gray-400">{"답변 대기"}</span>
           <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
         </div>
-        <span className="mx-10 border-l border-gray-400" />
-        <div className="flex items-center gap-4">
-          <span className="h3 font-bold text-gray-600">{"답변 대기"}</span>
-          <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
-        </div>
-        <span className="mx-10 border-l border-gray-400" />
-        <div className="flex items-center gap-4">
-          <span className="h3 font-bold text-gray-600">{"답변 완료"}</span>
-          <span className="h1 font-extrabold text-brand-orange">{`${0}건`}</span>
+        <span className="mx-8 border-l border-gray-700" />
+        <div className="flex-1 flex flex-col gap-4">
+          <span className="h4 font-medium text-gray-400">{"답변 완료"}</span>
+          <span className="h1 font-extrabold text-gray-0">{`${0}건`}</span>
         </div>
       </div>
     </div>
