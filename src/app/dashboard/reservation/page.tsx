3b8d9f473310b50dff02f837a6de2014110c89d2
Hailey Kim <haileydeveloper@gmail.com>
2024-07-17 15:17:58 +0900

    feat: create /dashboard/reservation page

--- src/app/dashboard/reservation/page.tsx
+++ src/app/dashboard/reservation/page.tsx
@@ -1,3 +1,13 @@
+import AdBanners from "@/components/dashboard/reservation/molecules/AdBanner";
+import ItemList from "@/components/dashboard/reservation/organisms/ItemList";
+import ItemSearch from "@/components/dashboard/reservation/organisms/ItemSearch";
+
 export default function ReservationPage() {
-  return <div></div>;
+  return (
+    <>
+      <AdBanners />
+      <ItemSearch />
+      <ItemList />
+    </>
+  );
 }
