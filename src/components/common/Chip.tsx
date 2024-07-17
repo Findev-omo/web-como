6f6a8b67e754010caa6c20eaf9f1bc7bca20ee35
Hailey Kim <haileydeveloper@gmail.com>
2024-07-17 15:16:59 +0900

    feat: add more features to Chip component

--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
 interface Props {
   content: string;
+  primary?: boolean;
+  padding?: string;
+  onClick?: () => void;
 }
 
-export default function Chip(props: Props) {
+export default function Chip({ padding = "py-0.5 px-3.5", ...props }: Props) {
   return (
-    <div className="py-0.5 px-3.5 rounded-full body-1 font-medium text-gray-50 bg-gray-800 select-none">
+    <div
+      className={cn(
+        "w-fit rounded-full body-1 font-medium select-none",
+        props.primary
+          ? "text-gray-50 bg-gray-800"
+          : "border border-gray-400 text-gray-800 bg-gray-50",
+        props.onClick ? "cursor-pointer" : "",
+        padding
+      )}
+      onClick={props.onClick}
+    >
       {props.content}
     </div>
   );
