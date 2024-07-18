7c6fe4a4e12a25eec3eed6e60016c37533ea45a2
Hailey Kim <haileydeveloper@gmail.com>
2024-07-19 08:39:46 +0900

    feat: create AnnouncementList

--- src/app/dashboard/announcement/page.tsx
+++ src/app/dashboard/announcement/page.tsx
@@ -1,11 +1,13 @@
 import AnnouncementOverview from "@/components/dashboard/announcement/organisms/AnnouncementOverview";
 import AnnouncementSearch from "@/components/dashboard/announcement/organisms/AnnouncementSearch";
+import AnnouncementList from "@/components/dashboard/announcement/organisms/AnnouncementList";
 
 export default function AnnouncementPage() {
   return (
     <>
       <AnnouncementOverview />
       <AnnouncementSearch />
+      <AnnouncementList />
     </>
   );
 }
