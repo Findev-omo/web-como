9b7b3c7d8261fba125aae3aa301522325689b456
Hailey Kim <haileydeveloper@gmail.com>
2024-07-18 12:13:52 +0900

    feat: create layout, update page

--- src/app/dashboard/expanse/page.tsx
+++ src/app/dashboard/expanse/page.tsx
@@ -1,3 +1,11 @@
+import ApplicationGuide from "@/components/dashboard/expanse/molecules/ApplicationGuide";
+import ExpanseSearch from "@/components/dashboard/expanse/organisms/ExpanseSearch";
+
 export default function ExpansePage() {
-  return <div></div>;
+  return (
+    <>
+      <ApplicationGuide />
+      <ExpanseSearch />
+    </>
+  );
 }
