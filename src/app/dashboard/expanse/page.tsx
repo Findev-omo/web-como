bbb3d7ca6c6facb99aa29d7d5b703f53e2d9bc21
Hailey Kim <haileydeveloper@gmail.com>
2024-07-19 20:42:49 +0900

    feat: update ExpansePage (add Overview)

--- src/app/dashboard/expanse/page.tsx
+++ src/app/dashboard/expanse/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import ApplicationGuide from "@/components/dashboard/expanse/molecules/ApplicationGuide";
 import ExpanseList from "@/components/dashboard/expanse/organisms/ExpanseList";
+import ExpanseOverview from "@/components/dashboard/expanse/organisms/ExpanseOverview";
 import ExpanseSearch, {
   type ExpanseSearchFilter,
 } from "@/components/dashboard/expanse/organisms/ExpanseSearch";
@@ -18,6 +19,7 @@ export default function ExpansePage() {
   return (
     <>
       <ApplicationGuide />
+      <ExpanseOverview />
       <ExpanseSearch
         currentFilter={currentFilter}
         handleChangeFilter={handleChangeFilter}
