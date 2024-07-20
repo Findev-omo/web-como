d6c1378f76ad20d37ceccf80df655b7cf4a536ce
Hailey Kim <haileydeveloper@gmail.com>
2024-07-20 20:48:31 +0900

    feat: update ReportOverview and Viewer

--- src/components/dashboard/report/organisms/ReportViewer.tsx
+++ src/components/dashboard/report/organisms/ReportViewer.tsx
@@ -4,7 +4,7 @@ export default function ReportViewer() {
   return (
     <div className="flex flex-col gap-3 p-8 rounded-xl bg-gray-50">
       <h3 className="font-semibold text-gray-900">{"작성한 보고서"}</h3>
-      <PDFViewer />
+      <PDFViewer file={"../../../sample.pdf"} />
     </div>
   );
 }
