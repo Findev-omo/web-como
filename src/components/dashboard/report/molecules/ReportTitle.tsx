013c4d4ff951d6bb17c57b9135bd6aa009a3c619
Hailey Kim <haileydeveloper@gmail.com>
2024-07-20 20:48:43 +0900

    feat: create ReportTitle

---
+++ src/components/dashboard/report/molecules/ReportTitle.tsx
@@ -0,0 +1,10 @@
+export default function ReportTitle() {
+  return (
+    <div className="space-y-2 p-6 rounded-xl bg-gray-0">
+      <h2 className="font-bold text-gray-900">{"활동 내역"}</h2>
+      <div className="p-3 rounded-md border border-gray-400 h4 font-medium text-gray-900 bg-gray-50">
+        {"2024_05_26 어푸어푸 수영 모임 (2)"}
+      </div>
+    </div>
+  );
+}
