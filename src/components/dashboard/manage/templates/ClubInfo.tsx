8509e47c7236629b9943c2c9ea4654825ad32ba3
Hailey Kim <haileydeveloper@gmail.com>
2024-07-23 09:42:32 +0900

    refactor: separate MapSearch from /manage ClubInfo tab

--- src/components/dashboard/manage/templates/ClubInfo.tsx
+++ src/components/dashboard/manage/templates/ClubInfo.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
-import { usePlaceSearch } from "@/app/api/map/hook";
-import { useGeocode } from "@/app/api/map/hook";
 import { closeModal, cn, openModal } from "@/lib/utils";
 import Backdrop from "@/components/common/Backdrop";
 import Input from "@/components/common/Input";
-import NaverMap from "@/components/dashboard/common/Map";
-import { Edit, Search } from "@/assets/icons/util";
+import { Edit } from "@/assets/icons/util";
 import { Calendar } from "@/assets/icons/info";
-import { Close, Remove } from "@/assets/icons/action";
+import { Close } from "@/assets/icons/action";
+import MapPlaceSearch from "../organisms/MapPlaceSearch";
 
 const image = null;
 
@@ -20,53 +18,6 @@ export default function ClubInfoTab() {
     iteration?: string;
     time: string;
   }>({ time: "19:00" });
-  const [selectedPlace, setSelectedPlace] = useState<{
-    roadAddress: string;
-    title?: string;
-  }>();
-
-  const [searchResult, setSearchResult] = useState<
-    {
-      roadAddress: string;
-      title?: string;
-    }[]
-  >();
-  const [closeSearchResult, setCloseSearchResult] = useState<boolean>(false);
-  const [searchTerm, setSearchTerm] = useState<string>();
-  const [query, setQuery] = useState<string>();
-  const { data: placeData } = usePlaceSearch(searchTerm);
-  const { data: geocodeData } = useGeocode(query);
-
-  useEffect(() => {
-    if (placeData) {
-      //   console.log(placeData);
-      if (placeData.items.length < 1) {
-        setQuery(searchTerm);
-      }
-    }
-  }, [placeData, searchTerm]);
-
-  useEffect(() => {
-    if (placeData) {
-      if (placeData.items.length > 0) {
-        return setSearchResult(
-          placeData.items.map((item) => {
-            return { roadAddress: item.roadAddress, title: item.title };
-          })
-        );
-      }
-    }
-    if (geocodeData) {
-      console.log(geocodeData);
-      if (geocodeData.meta.totalCount > 0) {
-        return setSearchResult(
-          geocodeData.addresses.map((item) => {
-            return { roadAddress: item.roadAddress };
-          })
-        );
-      }
-    }
-  }, [placeData, geocodeData]);
 
   return (
     <form className="flex gap-3">
@@ -222,103 +173,7 @@ export default function ClubInfoTab() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <span className="h3 font-semibold text-gray-900">
-              {"활동 장소"}
-            </span>
-            <div className="relative">
-              <div className="flex gap-3">
-                <div className="flex items-center gap-3 w-3/5 h-[60px] px-3 rounded-md border border-gray-100 has-[:focus-visible]:border-gray-900 bg-gray-100 has-[:focus-visible]:bg-gray-50 transition duration-300">
-                  <Search className="w-5 h-5 text-gray-500" />
-                  <input
-                    type="text"
-                    name="roadAddress"
-                    id="roadAddress"
-                    placeholder="활동 장소를 검색해주세요"
-                    className="peer w-full h4 font-medium outline-none placeholder:text-gray-400 text-gray-900 bg-transparent transition duration-300"
-                    value={searchTerm}
-                    onChange={(e) => {
-                      setSearchTerm(e.target.value);
-                      setCloseSearchResult(false);
-                    }}
-                  />
-                  {selectedPlace?.roadAddress && (
-                    <button
-                      type="button"
-                      onClick={() => {
-                        setSelectedPlace({ roadAddress: "", title: "" });
-                        setSearchTerm("");
-                      }}
-                    >
-                      <Remove className="w-5 h-5 text-gray-500 cursor-pointer select-none" />
-                    </button>
-                  )}
-                </div>
-                <div className="flex items-center w-2/5 h-[60px] px-3 rounded-md border border-gray-100 has-[:focus-visible]:border-gray-900 bg-gray-100 has-[:focus-visible]:bg-gray-50 transition duration-300">
-                  <input
-                    type="text"
-                    name="detailAddress"
-                    id="detailAddress"
-                    className="peer w-full h4 font-medium outline-none placeholder:text-gray-400 text-gray-900 bg-transparent transition duration-300"
-                    value={selectedPlace?.title}
-                    onChange={(e) => {
-                      if (selectedPlace?.roadAddress) {
-                        setSelectedPlace((prev) => {
-                          return {
-                            title: e.target.value,
-                            roadAddress: prev!.roadAddress,
-                          };
-                        });
-                      }
-                    }}
-                  />
-                </div>
-              </div>
-              {searchResult && (
-                <div
-                  className={cn(
-                    "absolute z-20 flex flex-col gap-3 w-3/5 mt-1 p-6 rounded-xl bg-gray-50 shadow",
-                    closeSearchResult ? "hidden" : "block"
-                  )}
-                >
-                  {searchResult.map((item, i) => (
-                    <div
-                      key={item.roadAddress + item.title}
-                      className={cn(
-                        "flex flex-col gap-0.5 border-gray-200 cursor-pointer select-none",
-                        i === searchResult.length - 1 ? "" : "pb-3 border-b"
-                      )}
-                      onClick={() => {
-                        setSelectedPlace({
-                          roadAddress: item.roadAddress,
-                          title: item.title
-                            ?.replaceAll("<b>", "")
-                            .replaceAll("</b>", ""),
-                        });
-                        setCloseSearchResult(true);
-                        setSearchTerm(item.roadAddress);
-                      }}
-                    >
-                      <p
-                        className="body-1 font-semibold text-brand-orange"
-                        dangerouslySetInnerHTML={{
-                          __html: item.title || item.roadAddress,
-                        }}
-                      />
-                      {item.title && (
-                        <span className="body-2 font-medium text-gray-500">
-                          {item.roadAddress}
-                        </span>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-            <div className="w-full h-[70vh]">
-              <NaverMap query={selectedPlace?.roadAddress} />
-            </div>
-          </div>
+          <MapPlaceSearch />
         </div>
       </div>
     </form>
