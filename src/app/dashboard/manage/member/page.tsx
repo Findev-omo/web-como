5feae0c4301d9ba3d96e8a6b7e7cbd9f8a103635
Hailey Kim <haileydeveloper@gmail.com>
2024-07-23 11:10:19 +0900

    feat: create MemberList, update /dashboard/member page

--- src/app/dashboard/manage/member/page.tsx
+++ src/app/dashboard/manage/member/page.tsx
@@ -1,3 +1,13 @@
+import MemberTitle from "@/components/dashboard/member/molecules/MemberTitle";
+import MemberList from "@/components/dashboard/member/organisms/MemberList";
+import MemberSearch from "@/components/dashboard/member/organisms/MemberSearch";
+
 export default function ClubMemberPage() {
-  return <div></div>;
+  return (
+    <>
+      <MemberTitle />
+      <MemberSearch />
+      <MemberList />
+    </>
+  );
 }
