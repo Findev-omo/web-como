00a4ac1f5670ff97e7202b05be342107da0d5d5b
Hailey Kim <haileydeveloper@gmail.com>
2024-07-24 22:07:05 +0900

    feat: create AnnouncementDetail page and update BackButton

--- src/components/dashboard/common/BackButton.tsx
+++ src/components/dashboard/common/BackButton.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { ChevronLeftBackButton } from "@/assets/icons/chevron";
 import { useRouter } from "next/navigation";
+import { ChevronLeftBackButton } from "@/assets/icons/chevron";
 
 export default function BackButton() {
   const { back } = useRouter();
+  
   return (
     <button
-      className="flex items-center justify-center w-12 h-12 rounded-full border border-brand-orange bg-gray-50 cursor-pointer"
+      className="flex items-center justify-center w-12 h-12 rounded-full outline-none border border-brand-orange bg-gray-50 cursor-pointer"
       onClick={back}
     >
       <ChevronLeftBackButton className="w-5 h-6 text-brand-orange" />
