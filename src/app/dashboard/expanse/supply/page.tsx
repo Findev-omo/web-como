9e101a8ef880ec5783a5a7686c84b1a6d63cbdb9
Hailey Kim <haileydeveloper@gmail.com>
2024-07-25 14:17:11 +0900

    feat: update /expanse/supply page

--- src/app/dashboard/expanse/supply/page.tsx
+++ src/app/dashboard/expanse/supply/page.tsx
@@ -1,3 +1,13 @@
+import SupplyOverview from "@/components/dashboard/expanse/organisms/SupplyOverview";
+import SupplySearch from "@/components/dashboard/expanse/molecules/SupplySearch";
+import SupplyList from "@/components/dashboard/expanse/organisms/SupplyList";
+
 export default function SupplyPage() {
-  return <div></div>;
+  return (
+    <>
+      <SupplyOverview />
+      <SupplySearch />
+      <SupplyList />
+    </>
+  );
 }
