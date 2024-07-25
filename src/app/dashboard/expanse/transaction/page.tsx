1e939f9ae9e15bb5aecab4dc9deae1b6781e0275
Hailey Kim <haileydeveloper@gmail.com>
2024-07-25 11:04:01 +0900

    feat: create TransactionList and update /dashboard/expanse/transaction page

--- src/app/dashboard/expanse/transaction/page.tsx
+++ src/app/dashboard/expanse/transaction/page.tsx
@@ -1,9 +1,13 @@
-import BackButton from "@/components/dashboard/common/BackButton";
+import TransactionOverview from "@/components/dashboard/expanse/organisms/TransactionOverview";
+import TransactionSearch from "@/components/dashboard/expanse/molecules/TransactionSearch";
+import TransactionList from "@/components/dashboard/expanse/organisms/TransactionList";
 
 export default function TransactionPage() {
   return (
     <>
-      <BackButton />
+      <TransactionOverview />
+      <TransactionSearch />
+      <TransactionList />
     </>
   );
 }
