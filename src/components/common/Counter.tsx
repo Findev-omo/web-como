0b098c68d8b05537248cd555574c956812cded05
Hailey Kim <haileydeveloper@gmail.com>
2024-07-26 12:07:26 +0900

    feat: update Counter and OptionItem

--- src/components/common/Counter.tsx
+++ src/components/common/Counter.tsx
@@ -17,13 +17,13 @@ export default function Counter(props: Props) {
   return (
     <div
       className={cn(
-        "flex items-center w-fit h-9 rounded-lg border border-gray-300 bg-gray-50",
+        "flex items-center w-fit h-9 rounded-lg bg-gray-50",
         poppins.className
       )}
     >
       <button
         type="button"
-        className="flex items-center justify-center w-9 h-9 rounded-s-lg border-r border-gray-300 text-gray-900 disabled:text-gray-300 disabled:bg-gray-200"
+        className="flex items-center justify-center w-9 h-9 rounded-s-lg border border-gray-300 text-gray-900 disabled:text-gray-400 disabled:bg-gray-100"
         onClick={() => props.handleChange(props.currentValue - 1)}
         disabled={props.currentValue === props.minValue}
       >
@@ -34,14 +34,23 @@ export default function Counter(props: Props) {
         name="max"
         type="number"
         value={props.currentValue}
-        onChange={(e) => props.handleChange(parseInt(e.target.value))}
+        onChange={(e) => {
+          const newValue = parseInt(e.target.value);
+          if (newValue > props.maxValue) {
+            props.handleChange(props.maxValue);
+          } else if (newValue < props.minValue) {
+            props.handleChange(props.minValue);
+          } else {
+            props.handleChange(newValue);
+          }
+        }}
         min={props.minValue}
         max={props.maxValue}
-        className="w-14 h-full outline-none text-center body-1 font-medium text-gray-900"
+        className="w-14 h-full outline-none border-y border-gray-300 text-center body-1 font-medium text-gray-900"
       />
       <button
         type="button"
-        className="flex items-center justify-center w-9 h-9 rounded-e-lg border-l border-gray-300 text-gray-900 disabled:text-gray-300 disabled:bg-gray-200"
+        className="flex items-center justify-center w-9 h-9 rounded-e-lg border border-gray-300 text-gray-900 disabled:text-gray-400 disabled:bg-gray-100"
         onClick={() => props.handleChange(props.currentValue + 1)}
         disabled={props.currentValue === props.maxValue}
       >
