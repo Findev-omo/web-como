413d2b46b8caf0426fb6f734332acb753643c348
Hailey Kim <haileydeveloper@gmail.com>
2024-07-26 19:53:09 +0900

    feat: create Reservation List, update /reservation/manage page

--- src/app/dashboard/reservation/manage/page.tsx
+++ src/app/dashboard/reservation/manage/page.tsx
@@ -1,3 +1,15 @@
+import ReservationTitle from "@/components/dashboard/reservation/molecules/ReservationTitle";
+import ReservationOverview from "@/components/dashboard/reservation/organisms/ReservationOverview";
+import ReservationSearch from "@/components/dashboard/reservation/molecules/ReservationSearch";
+import ReservationList from "@/components/dashboard/reservation/organisms/ReservationList";
+
 export default function ReservationManagementPage() {
-  return <div></div>;
+  return (
+    <>
+      <ReservationTitle />
+      <ReservationOverview />
+      <ReservationSearch />
+      <ReservationList />
+    </>
+  );
 }
