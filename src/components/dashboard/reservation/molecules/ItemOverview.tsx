218139c60ca3284ffd50812c725dfbbcf5823445
Hailey Kim <haileydeveloper@gmail.com>
2024-07-26 11:12:45 +0900

    feat: update ItemOverview styles

--- src/components/dashboard/reservation/molecules/ItemOverview.tsx
+++ src/components/dashboard/reservation/molecules/ItemOverview.tsx
@@ -31,21 +31,21 @@ export default function ItemDetailOverview() {
           </div>
           <div className="flex gap-10 mt-8 body-2 font-medium text-gray-700">
             <div className="space-y-1">
-              <div className="flex gap-1">
+              <div className="flex items-center gap-1">
                 <Profile className="w-4 h-4" />
                 {"OMO전용 호스트"}
               </div>
-              <div className="flex gap-1">
+              <div className="flex items-center gap-1">
                 <Calendar className="w-4 h-4" />
                 {"5/1부터"}
               </div>
             </div>
             <div className="space-y-1">
-              <div className="flex gap-1">
+              <div className="flex items-center gap-1">
                 <Marker className="w-4 h-4" />
                 {"서울시 구로구"}
               </div>
-              <div className="flex gap-1">
+              <div className="flex items-center gap-1">
                 <People className="w-4 h-4" />
                 {"최대 30명"}
               </div>
@@ -59,11 +59,11 @@ export default function ItemDetailOverview() {
           <div className="flex items-center gap-2 w-fit py-8 pr-4 cursor-pointer select-none">
             <Avatar size="w-[56px] h-[56px]" />
             <div>
-              <div className="flex h4 font-bold text-gray-900">
+              <div className="flex items-center h4 font-bold text-gray-900">
                 {"호스트명"}
-                <ChevronRight className="w-6 h-6" />
+                <ChevronRight className="w-4 h-4 xl:w-6 xl:h-6" />
               </div>
-              <div className="flex mt-[3px] caption-1 font-medium text-gray-500">
+              <div className="flex items-center mt-[3px] caption-1 font-medium text-gray-500">
                 <Category className="w-3.5 h-3.5" />
                 {"카테고리"}
               </div>
