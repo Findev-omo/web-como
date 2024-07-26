430b298065b8394b085f09e63297be30cfa05739
Hailey Kim <haileydeveloper@gmail.com>
2024-07-26 12:08:32 +0900

    feat: create /reservation/host page, add link to host page on ItemOverview

--- src/components/dashboard/reservation/molecules/ItemOverview.tsx
+++ src/components/dashboard/reservation/molecules/ItemOverview.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import Avatar from "@/components/common/Avatar";
 import Chip from "@/components/common/Chip";
@@ -56,19 +57,21 @@ export default function ItemDetailOverview() {
           <div className="w-full pb-2 border-b border-gray-200 h4 font-bold text-gray-900">
             {"호스트의 다른 상품 보기"}
           </div>
-          <div className="flex items-center gap-2 w-fit py-8 pr-4 cursor-pointer select-none">
-            <Avatar size="w-[56px] h-[56px]" />
-            <div>
-              <div className="flex items-center h4 font-bold text-gray-900">
-                {"호스트명"}
-                <ChevronRight className="w-4 h-4 xl:w-6 xl:h-6" />
-              </div>
-              <div className="flex items-center mt-[3px] caption-1 font-medium text-gray-500">
-                <Category className="w-3.5 h-3.5" />
-                {"카테고리"}
+          <Link href={`/dashboard/reservation/host/${1}`}>
+            <div className="flex items-center gap-2 w-fit py-8 pr-4 cursor-pointer select-none">
+              <Avatar size="w-[56px] h-[56px]" />
+              <div>
+                <div className="flex items-center h4 font-bold text-gray-900">
+                  {"호스트명"}
+                  <ChevronRight className="w-4 h-4 xl:w-6 xl:h-6" />
+                </div>
+                <div className="flex items-center mt-[3px] caption-1 font-medium text-gray-500">
+                  <Category className="w-3.5 h-3.5" />
+                  {"카테고리"}
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
