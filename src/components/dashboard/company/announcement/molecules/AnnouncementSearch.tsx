56dadb02e1ac8a5876e4fc4cfc4f8fb67ebf311d
Hailey Kim <haileydeveloper@gmail.com>
2024-07-29 12:05:34 +0900

    feat: create /dashboard/company/announcement Search component and page

---
+++ src/components/dashboard/company/announcement/molecules/AnnouncementSearch.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useState } from "react";
+import SearchBarWithFilterChips from "@/components/dashboard/common/SearchBarWithFilterChips";
+import {
+  initialSearchValueWithFilter,
+  SearchValueWithFilter,
+} from "@/lib/types/search";
+
+const filterList = [
+  { name: "전체 보기", value: "all" },
+  {
+    name: (
+      <>
+        <span className="font-poppins font-bold">{"omo"}</span>
+        {" 공지"}
+      </>
+    ),
+    value: "omo",
+  },
+  {
+    name: (
+      <>
+        <span className="font-poppins font-bold">{"omo "}</span>
+        <span className="font-cochin font-bold text-xl leading-6">
+          {"shop"}
+        </span>
+        {" 공지"}
+      </>
+    ),
+    value: "omo-shop",
+  },
+];
+
+export default function AnnouncementSearch() {
+  const [currentSearchValue, setCurrentSearchValue] =
+    useState<SearchValueWithFilter>(initialSearchValueWithFilter);
+
+  const handleSearch = () => {};
+
+  return (
+    <SearchBarWithFilterChips
+      filterList={filterList}
+      currentValue={currentSearchValue}
+      handleChange={({ term, filter }) =>
+        setCurrentSearchValue((prev) => {
+          return { term: term || prev.term, filter: filter || prev.filter };
+        })
+      }
+      handleSearch={handleSearch}
+    />
+  );
+}
