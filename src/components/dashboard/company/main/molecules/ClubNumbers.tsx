034d3517bb05fac6b2189bc5eb3d4a9d871cbfc5
Hailey Kim <haileydeveloper@gmail.com>
2024-07-30 09:41:46 +0900

    feat: create EmployeeShortcut, ClubNumbers, DownloadShortcut

---
+++ src/components/dashboard/company/main/molecules/ClubNumbers.tsx
@@ -0,0 +1,30 @@
+export default function ClubNumbers() {
+  return (
+    <div className="flex-grow-[2] flex items-center gap-6 py-8 px-10 rounded-xl bg-gray-0">
+      <div className="flex-1 space-y-4">
+        <div className="h4 font-medium text-gray-700 truncate">
+          {"신규 동호회"}
+        </div>
+        <div className="h1 font-extrabold text-gray-900 truncate">
+          {"123개"}
+        </div>
+      </div>
+      <div className="flex-1 space-y-4">
+        <div className="h4 font-medium text-gray-700 truncate">
+          {"탈퇴 동호회"}
+        </div>
+        <div className="h1 font-extrabold text-gray-900 truncate">
+          {"123개"}
+        </div>
+      </div>
+      <div className="flex-1 space-y-4">
+        <div className="h4 font-medium text-gray-700 truncate">
+          {"현재 사내동호회 수"}
+        </div>
+        <div className="h1 font-extrabold text-brand-orange truncate">
+          {"123개"}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/dashboard/company/main/molecules/EmployeeShortcut.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { COMPANY_DASHBOARD_ENDPOINT } from "@/lib/constants";
+import { ChevronRight } from "@/assets/icons/chevron";
+
+export default function EmployeeShortcut() {
+  return (
+    <Link
+      href={`${COMPANY_DASHBOARD_ENDPOINT}/employee`}
+      className="flex-1 space-y-4 max-w-[390px] p-8 rounded-xl bg-gray-0 select-none"
+    >
+      <div className="h4 font-medium text-gray-700 truncate">
+        {"전체 임직원 수"}
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="h1 font-extrabold text-gray-900 truncate">
+          {"1234명"}
+        </div>
+        <ChevronRight className="w-9 h-9 text-gray-600" />
+      </div>
+    </Link>
+  );
+}
