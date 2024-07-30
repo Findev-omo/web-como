29bfd7648e6a6169104d29c94ea7a26c3cdeb25b
Hailey Kim <haileydeveloper@gmail.com>
2024-07-30 18:47:29 +0900

    feat: create ShopStats tabs

--- src/components/dashboard/company/main/molecules/ShopStatsSearchRanking.tsx
+++ src/components/dashboard/company/main/molecules/ShopStatsSearchRanking.tsx
@@ -1,3 +1,44 @@
+import { cn } from "@/lib/utils";
+
 export default function ShopStatsSearchRanking() {
-  return <div></div>;
+  return (
+    <div className="flex h-full">
+      <ul className="flex-1 flex flex-col h-full">
+        {Array.from({ length: 5 }).map((item, i) => (
+          <li
+            key={i}
+            className="flex-1 flex border-b border-gray-400 body-1 font-medium text-gray-800"
+          >
+            <div
+              className={cn(
+                "flex items-center justify-center w-16",
+                i === 0 ? "text-brand-orange" : ""
+              )}
+            >
+              {i + 1}
+            </div>
+            <div
+              className={cn(
+                "flex-1 flex items-center font-bold",
+                i === 0 ? "text-brand-orange" : ""
+              )}
+            >
+              {"검색어"}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex-1 flex flex-col h-full">
+        {Array.from({ length: 5 }).map((item, i) => (
+          <li
+            key={i}
+            className="flex-1 flex border-b border-gray-400 body-1 font-medium text-gray-800"
+          >
+            <div className="flex items-center justify-center w-16">{i + 6}</div>
+            <div className="flex-1 flex items-center font-bold">{"검색어"}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
