9aa4e95d127fe1ce29eed7b65b36cb0aef54013f
Hailey Kim <haileydeveloper@gmail.com>
2024-07-30 18:36:04 +0900

    feat: remove underline on hover on Overview

--- src/components/dashboard/company/main/molecules/Overview.tsx
+++ src/components/dashboard/company/main/molecules/Overview.tsx
@@ -7,7 +7,7 @@ export default function DashboardOverview() {
           <h4 className="font-medium text-gray-400">
             {"이번 주 omo 이용 건수"}
           </h4>
-          <span className="h1 font-extrabold text-brand-orange underline underline-offset-4 decoration-gray-800 hover:decoration-brand-orange transition duration-300">
+          <span className="h1 font-extrabold text-brand-orange">
             {`${0}건`}
           </span>
         </div>
@@ -16,19 +16,19 @@ export default function DashboardOverview() {
           <h4 className="font-medium text-gray-400">
             {"오늘 퇴근 후 omo 이용 건수"}
           </h4>
-          <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">{`${0}건`}</span>
+          <span className="h1 font-extrabold text-gray-0">{`${0}건`}</span>
         </div>
         <span className="h-[104px] border-l border-gray-700" />
         <div className="flex-1 flex flex-col gap-4 py-3 px-2">
           <h4 className="font-medium text-gray-400">
             {"신규 동호회 신청 건수"}
           </h4>
-          <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">{`${0}건`}</span>
+          <span className="h1 font-extrabold text-gray-0">{`${0}건`}</span>
         </div>
         <span className="h-[104px] border-l border-gray-700" />
         <div className="flex-1 flex flex-col gap-4 py-3 px-2">
           <h4 className="font-medium text-gray-400">{"잔여 복지포인트"}</h4>
-          <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">{`${0}건`}</span>
+          <span className="h1 font-extrabold text-gray-0">{`${0}건`}</span>
         </div>
       </div>
     </div>
