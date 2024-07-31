3583ab70f7b18c600666f2647349b148e21e832a
Hailey Kim <haileydeveloper@gmail.com>
2024-07-31 20:42:31 +0900

    feat: create ClubOverview

--- src/app/dashboard/company/club/page.tsx
+++ src/app/dashboard/company/club/page.tsx
@@ -1,3 +1,13 @@
+import ClubOverview from "@/components/dashboard/company/club/molecules/ClubOverview";
+import ApplicantProfileModal from "@/components/dashboard/company/club/molecules/ApplicantProfileModal";
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <ClubOverview />
+      <div className="mt-0">
+        <ApplicantProfileModal />
+      </div>
+    </>
+  );
 }
---
+++ src/components/dashboard/company/club/molecules/ClubOverview.tsx
@@ -0,0 +1,25 @@
+export default function ClubOverview() {
+  return (
+    <div className="flex flex-col gap-6 h-fit p-8 rounded-xl bg-gray-800 select-none">
+      <h2 className="h1 font-bold text-gray-0">{"사내동호회 관리"}</h2>
+      <div className="flex gap-8 truncate">
+        <div className="flex-1 flex flex-col gap-4 py-3 px-2">
+          <h4 className="font-medium text-gray-400">{"해체신정"}</h4>
+          <span className="h1 font-extrabold text-brand-orange">
+            {`${2}건`}
+          </span>
+        </div>
+        <span className="h-[104px] border-l border-gray-700" />
+        <div className="flex-1 flex flex-col gap-4 py-3 px-2">
+          <h4 className="font-medium text-gray-400">{"활동중"}</h4>
+          <span className="h1 font-extrabold text-gray-0">{`${6}건`}</span>
+        </div>
+        <span className="h-[104px] border-l border-gray-700" />
+        <div className="flex-1 flex flex-col gap-4 py-3 px-2">
+          <h4 className="font-medium text-gray-400">{"해체완료"}</h4>
+          <span className="h1 font-extrabold text-gray-0">{`${3}건`}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
