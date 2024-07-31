baeecc945fbfcb1f695ab61c4525a958853d4434
Hailey Kim <haileydeveloper@gmail.com>
2024-07-31 20:39:35 +0900

    feat: create ApplicationDetail page

--- src/app/dashboard/company/club/new/[id]/page.tsx
+++ src/app/dashboard/company/club/new/[id]/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-  return <></>;
+"use client";
+
+import { openModal } from "@/lib/utils";
+import BackButton from "@/components/dashboard/common/BackButton";
+import PDFViewer from "@/components/dashboard/club/common/PDFViewer";
+import RejectApplicationModal from "@/components/dashboard/company/club/molecules/RejectApplicationModal";
+import RevertRejectionModal from "@/components/dashboard/company/club/molecules/RevertRejectionModal";
+
+export default function ApplicationDetailPage() {
+  return (
+    <>
+      <BackButton />
+      <div className="space-y-3 p-8 rounded-xl bg-gray-0">
+        <div className="flex items-center justify-between">
+          <h2 className="h3 font-semibold text-gray-900">{"작성한 신청서"}</h2>
+          <div className="flex gap-2">
+            <button className="py-1 px-4 rounded body-1 font-medium text-gray-50 bg-point-blue">
+              {"승인"}
+            </button>
+            <button
+              className="py-1 px-4 rounded body-1 font-medium text-gray-50 bg-gray-600"
+              onClick={() => openModal("reject-application")}
+            >
+              {"반려"}
+            </button>
+          </div>
+        </div>
+        <PDFViewer file="../../../../../sample.pdf" />
+      </div>
+      <div className="mt-0">
+        <RejectApplicationModal />
+        <RevertRejectionModal />
+      </div>
+    </>
+  );
 }
