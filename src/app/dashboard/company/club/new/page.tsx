79880b08ea1f6aef5727f1b818c86777fb23fd33
Hailey Kim <haileydeveloper@gmail.com>
2024-07-31 14:19:21 +0900

    feat: create /club/new Overview

--- src/app/dashboard/company/club/new/page.tsx
+++ src/app/dashboard/company/club/new/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <></>;
+import ApplicationOverview from "@/components/dashboard/company/club/molecules/ApplicationOverview";
+
+export default function NewApplicationPage() {
+  return (
+    <>
+      <ApplicationOverview />
+    </>
+  );
 }
