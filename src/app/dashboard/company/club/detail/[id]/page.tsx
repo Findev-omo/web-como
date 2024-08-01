d47112249458c31aeba424a2614a83f1800bde8a
Hailey Kim <haileydeveloper@gmail.com>
2024-08-01 20:53:43 +0900

    feat: change tab=detail to tab=about

--- src/app/dashboard/company/club/detail/[id]/page.tsx
+++ src/app/dashboard/company/club/detail/[id]/page.tsx
@@ -3,12 +3,12 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import BackButton from "@/components/dashboard/common/BackButton";
 import ClubDetailMenuTabs from "@/components/dashboard/company/club/molecules/ClubDetailMenuTabs";
-import ClubDetailTabView from "@/components/dashboard/company/club/templates/ClubDetailTabView";
+import ClubDetailAboutTabView from "@/components/dashboard/company/club/templates/ClubDetailAboutTabView";
 import ClubDetailActivityTabView from "@/components/dashboard/company/club/templates/ClubDetailActivityTabView";
 import ClubDetailPictureTabView from "@/components/dashboard/company/club/templates/ClubDetailPictureTabView";
 import ClubDetailAttendanceTabView from "@/components/dashboard/company/club/templates/ClubDetailAttendanceTabView";
 
-export type ClubDetailMenu = "detail" | "activity" | "picture" | "attendance";
+export type ClubDetailMenu = "about" | "activity" | "picture" | "attendance";
 
 export interface ClubDetailMenuTab {
   name: string;
@@ -16,7 +16,7 @@ export interface ClubDetailMenuTab {
 }
 
 const tabList: ClubDetailMenuTab[] = [
-  { name: "동호회 상세", value: "detail" },
+  { name: "동호회 상세", value: "about" },
   { name: "활동 내역", value: "activity" },
   { name: "활동 사진", value: "picture" },
   { name: "출석부", value: "attendance" },
@@ -24,8 +24,8 @@ const tabList: ClubDetailMenuTab[] = [
 
 const renderCurrentTabPage = (currentTab: ClubDetailMenu) => {
   switch (currentTab) {
-    case "detail":
-      return <ClubDetailTabView />;
+    case "about":
+      return <ClubDetailAboutTabView />;
     case "activity":
       return <ClubDetailActivityTabView />;
     case "picture":
@@ -39,7 +39,7 @@ export default function Page() {
   const { push } = useRouter();
   const pathname = usePathname();
   const currentTab = (useSearchParams().get("tab") ||
-    "detail") as ClubDetailMenu;
+    "about") as ClubDetailMenu;
 
   const handleTabChange = (value: ClubDetailMenu) => {
     push(`${pathname}?tab=${value}`);
