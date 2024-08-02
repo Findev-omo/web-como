66d320e1bff32ff07ebd846260fb50f54e0168d9
Hailey Kim <haileydeveloper@gmail.com>
2024-08-02 18:09:02 +0900

    feat: create /employee/announcement/* pages

--- src/app/dashboard/company/employee/announcement/new/page.tsx
+++ src/app/dashboard/company/employee/announcement/new/page.tsx
@@ -1,3 +1,11 @@
+import BackButton from "@/components/dashboard/common/BackButton";
+import NewAnnouncementForm from "@/components/dashboard/shared/organisms/NewAnnouncementForm";
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <BackButton />
+      <NewAnnouncementForm />
+    </>
+  );
 }
