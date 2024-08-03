45fb9a2cb53c16df14f8bb8d9a03d4512c96ae62
Hailey Kim <haileydeveloper@gmail.com>
2024-08-03 20:47:17 +0900

    feat: update middleware and constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,6 @@
 export const HEADER_HEIGHT = "60px";
 
-export const CLUB_DASHBOARD_ENDPOINT = "/dashboard/club";
-export const COMPANY_DASHBOARD_ENDPOINT = "/dashboard/company";
+export const LOGIN_ENDPOINT = "/login";
+
+export const CLUB_DASHBOARD_ENDPOINT = "/club/dashboard";
+export const COMPANY_DASHBOARD_ENDPOINT = "/company/dashboard";
