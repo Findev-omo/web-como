1d41685580b9977623278e27d3b8aaf3f01e71ab
Hailey Kim <haileydeveloper@gmail.com>
2024-08-04 20:09:01 +0900

    feat: change DownloadShortcut to DownloadFigure

--- src/components/dashboard/company/main/organisms/EmployeeClubFigures.tsx
+++ src/components/dashboard/company/main/organisms/EmployeeClubFigures.tsx
@@ -1,13 +1,13 @@
 import EmployeeShortcut from "@/components/dashboard/company/main/molecules/EmployeeShortcut";
 import ClubFigures from "@/components/dashboard/company/main/molecules/ClubFigures";
-import DownloadShortcut from "@/components/dashboard/company/main/molecules/DownloadShortcut";
+import DownloadFigure from "@/components/dashboard/company/main/molecules/DownloadFigure";
 
 export default function EmployeeClubFigures() {
   return (
     <div className="flex gap-3">
       <EmployeeShortcut />
       <ClubFigures />
-      <DownloadShortcut />
+      <DownloadFigure />
     </div>
   );
 }
