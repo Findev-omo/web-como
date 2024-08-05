5dc8cc40a720d48d9bd7331fd864813a528155d0
Hailey Kim <haileydeveloper@gmail.com>
2024-08-05 14:26:36 +0900

    fix: typo in HostReview

--- src/components/dashboard/club/reservation/organisms/HostReview.tsx
+++ src/components/dashboard/club/reservation/organisms/HostReview.tsx
@@ -4,7 +4,7 @@ export default function HostReview() {
       <h3 className="h2 font-semibold text-gray-900">{"후기"}</h3>
       <p className="h3 font-medium text-gray-500">
         {
-          "아직 후기가 존재하지 않습니다. 활동에 참여하고 첫번째로 후기를 남겨보세요!"
+          "아직 후기가 존재하지 않습니다. 활동에 참여하고 첫 번째로 후기를 남겨보세요!"
         }
       </p>
     </div>
