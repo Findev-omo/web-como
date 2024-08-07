05f7f0b5d05210d866ca2c33fbdd4f76400898ee
Hailey Kim <haileydeveloper@gmail.com>
2024-08-07 16:47:25 +0900

    fix: change modal id

--- src/components/dashboard/company/club/modals/CancelForceDisbandModal.tsx
+++ src/components/dashboard/company/club/modals/CancelForceDisbandModal.tsx
@@ -6,7 +6,7 @@ import Button from "@/components/common/Button";
 
 export default function CancelForceDisbandModal() {
   return (
-    <div id="report-confirm" className="hidden modal">
+    <div id="cancel-disband" className="hidden modal">
       <Backdrop />
       <div className="fixed bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2 z-40 space-y-8 w-full max-w-[524px] p-8 rounded-xl bg-gray-0 shadow">
         <h2 className="text-center h1 font-bold text-gray-900">
