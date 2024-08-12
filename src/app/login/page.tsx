afe3273a993efce3b8b94e08411ebcc6d0ea9cc0
Hailey Kim <haileydeveloper@gmail.com>
2024-08-12 19:53:49 +0900

    fix: mobile view (login page)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,9 @@
 import LoginForm from "@/components/login/organisms/LoginForm";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className="fixed bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2">
+      <LoginForm />
+    </div>
+  );
 }
