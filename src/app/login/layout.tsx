8b32ce7b9e56e618ad94b243ed856353d6d87ebf
Hailey Kim <haileydeveloper@gmail.com>
2024-08-12 20:06:00 +0900

    fix: mobile view (login page)

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -13,7 +13,7 @@ export default function LoginLayout({
   return (
     <>
       <Header initialIsMobile={initialIsMobile} />
-      <main className="flex items-center justify-center min-h-screen h-full">
+      <main className="fixed bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2">
         {children}
       </main>
       <ShortFooter />
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,5 @@
 import LoginForm from "@/components/login/organisms/LoginForm";
 
 export default function LoginPage() {
-  return (
-    <div className="fixed bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2">
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 }
