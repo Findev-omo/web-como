7ee4a0d3df413a1ce3a4753356c7f57e6625e2e4
Hailey Kim <haileydeveloper@gmail.com>
2024-08-15 07:46:22 +0900

    feat: update company shop pages

--- src/app/company/dashboard/shop/item/[id]/page.tsx
+++ src/app/company/dashboard/shop/item/[id]/page.tsx
@@ -1,17 +1,19 @@
 import BackButton from "@/components/dashboard/common/BackButton";
-import InquiryShortcut from "@/components/dashboard/shared/shop/molecules/InquiryShortcut";
 import ItemDetailOverview from "@/components/dashboard/shared/shop/molecules/ItemOverview";
 import ItemDetail from "@/components/dashboard/shared/shop/organisms/ItemDetail";
+import ReviewList from "@/components/dashboard/shared/shop/organisms/ReviewList";
+import InquiryShortcut from "@/components/dashboard/shared/shop/molecules/InquiryShortcut";
 import ReservationPanel from "@/components/dashboard/shared/shop/organisms/ReservationPanel";
 
 export default function ItemDetailPage() {
   return (
     <>
       <BackButton />
-      <ItemDetailOverview />
       <div className="flex items-start gap-3">
         <div className="flex-1 flex flex-col gap-3">
+          <ItemDetailOverview />
           <ItemDetail />
+          <ReviewList headingStyle="h4 font-bold" />
           <InquiryShortcut />
         </div>
         <ReservationPanel />
