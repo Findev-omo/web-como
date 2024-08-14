b2fbb8eac814b3cf670c6c6ea85a11157c70d818
Hailey Kim <haileydeveloper@gmail.com>
2024-08-14 20:49:02 +0900

    feat: update StarRating for size small

--- src/components/common/StarRating.tsx
+++ src/components/common/StarRating.tsx
@@ -2,10 +2,11 @@ import { cn } from "@/lib/utils";
 import { Star } from "@/assets/icons/action";
 
 interface Props {
-  name: string;
   currentValue: number;
-  label?: string;
+  name?: string;
   readonly?: boolean;
+  size?: "small" | "large";
+  label?: string;
   labelStyle?: string;
   handleChange?: (newRating: number) => void;
 }
@@ -14,31 +15,36 @@ export default function StarRating(props: Props) {
   const labelStyle = "h3 font-semibold text-gray-900";
 
   return (
-    <div className="flex-1 flex flex-col gap-2">
+    <div className="flex flex-col gap-2">
       {props.label && (
         <label htmlFor={props.name} className={props.labelStyle || labelStyle}>
           {props.label}
         </label>
       )}
-      <div>
+      {!props.readonly && (
         <input id={props.name} name={props.name} type="number" hidden />
-        <div className="flex gap-1.5">
-          {Array.from({ length: 5 }).map((e, i) => (
-            <Star
-              key={i + 1}
-              className={cn(
-                "w-10 h-10",
-                props.currentValue >= i + 1
-                  ? "text-brand-orange"
-                  : "text-gray-300",
-                props.readonly ? "" : "cursor-pointer transition duration-200"
-              )}
-              onClick={
-                props.readonly ? undefined : () => props.handleChange!(i + 1)
-              }
-            />
-          ))}
-        </div>
+      )}
+      <div
+        className={cn(
+          "flex",
+          props.size === "small" ? "gap-1 my-[2.5px]" : "gap-1.5"
+        )}
+      >
+        {Array.from({ length: 5 }).map((e, i) => (
+          <Star
+            key={i + 1}
+            className={cn(
+              props.size === "small" ? "w-4 h-4" : "w-10 h-10",
+              props.currentValue >= i + 1
+                ? "text-brand-orange"
+                : "text-gray-300",
+              props.readonly ? "" : "cursor-pointer transition duration-200"
+            )}
+            onClick={
+              props.readonly ? undefined : () => props.handleChange!(i + 1)
+            }
+          />
+        ))}
       </div>
     </div>
   );
