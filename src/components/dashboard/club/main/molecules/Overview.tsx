82af3e4bb74d097f46eac49fd4b7281e90282bf9
Hailey Kim <haileydeveloper@gmail.com>
2024-08-15 22:01:55 +0900

    feat: update Overview with api

--- src/components/dashboard/club/main/molecules/Overview.tsx
+++ src/components/dashboard/club/main/molecules/Overview.tsx
@@ -1,7 +1,20 @@
 import Link from "next/link";
+import { getData } from "@/api/action";
+import { getClubId } from "@/lib/cookies";
 import { CLUB_DASHBOARD_ENDPOINT } from "@/lib/constants";
+import type { NotificationData } from "@/api/types/club/notification";
 
-export default function DashboardOverview() {
+export default async function DashboardOverview() {
+  const id = await getClubId();
+
+  if (!id) {
+    return;
+  }
+
+  const data: NotificationData = await getData(
+    `/v2/club/web/notification/${id}`
+  );
+
   return (
     <div className="col-span-4 flex flex-col gap-6 h-fit p-8 rounded-xl bg-gray-800 select-none">
       <h3 className="h1 font-bold text-gray-0">{"동호회 이름 주요 알림"}</h3>
@@ -12,7 +25,7 @@ export default function DashboardOverview() {
           </span>
           <Link href={`${CLUB_DASHBOARD_ENDPOINT}/manage/member?filter=new`}>
             <span className="h1 font-extrabold text-brand-orange underline underline-offset-4 decoration-gray-800 hover:decoration-brand-orange transition duration-300">
-              {`${0}건`}
+              {`${data.newClubApplications}건`}
             </span>
           </Link>
         </div>
@@ -22,7 +35,9 @@ export default function DashboardOverview() {
             {"동호회 문의 접수"}
           </span>
           <Link href={`${CLUB_DASHBOARD_ENDPOINT}/manage?tab=qna`}>
-            <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">{`${0}건`}</span>
+            <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">
+              {`${data.newClubInquiry}건`}
+            </span>
           </Link>
         </div>
         <span className="h-[104px] border-l border-gray-700" />
@@ -31,7 +46,9 @@ export default function DashboardOverview() {
             {"읽지 않은 인사 공지사항"}
           </span>
           <Link href={`${CLUB_DASHBOARD_ENDPOINT}/announcement?filter=company`}>
-            <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">{`${0}건`}</span>
+            <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">
+              {`${data.unreadExecutiveNotice}건`}
+            </span>
           </Link>
         </div>
         <span className="h-[104px] border-l border-gray-700" />
@@ -40,7 +57,9 @@ export default function DashboardOverview() {
             {"읽지 않은 omo 공지사항"}
           </span>
           <Link href={`${CLUB_DASHBOARD_ENDPOINT}/announcement?filter=omo`}>
-            <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">{`${0}건`}</span>
+            <span className="h1 font-extrabold text-gray-0 underline underline-offset-4 decoration-gray-800 hover:decoration-gray-0 transition duration-300">
+              {`${data.unreadOmoNotice}건`}
+            </span>
           </Link>
         </div>
       </div>
