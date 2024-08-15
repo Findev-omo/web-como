141066f6808e9f459e2a5610575cdd4a4801933d
Hailey Kim <haileydeveloper@gmail.com>
2024-08-15 21:46:15 +0900

    feat: create api action get method

---
+++ src/api/action.ts
@@ -0,0 +1,20 @@
+import { getAccessToken } from "@/lib/cookies";
+import type { IResponse } from "@/api/types/index";
+
+export const getData = async (endpoint: string) => {
+  const token = await getAccessToken();
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}${endpoint}`,
+    {
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const res: IResponse = await response.json();
+
+  return res.data;
+};
