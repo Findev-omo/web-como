cbe6799e401074b5b18bed8cc35a7af840b07252
Hailey Kim <haileydeveloper@gmail.com>
2024-08-16 17:13:57 +0900

    feat: update ClubPicture with api

--- src/components/dashboard/shared/organisms/ClubPictureItem.tsx
+++ src/components/dashboard/shared/organisms/ClubPictureItem.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import { openModal } from "@/lib/utils";
-import type { ClubPicture } from "@/components/dashboard/club/manage/templates/ClubPicture";
+import type { ActivityPictureData } from "@/api/types/club/activity";
 import Avatar from "@/components/common/Avatar";
 
 interface Props {
-  item: ClubPicture;
+  item: ActivityPictureData;
   readonly?: boolean;
 }
 
@@ -14,12 +15,12 @@ export default function ClubPictureItem({ item, readonly }: Props) {
     <div className="space-y-6 p-8 rounded-2xl bg-gray-0">
       <div className="flex items-start justify-between">
         <div className="flex items-center gap-4">
-          <Avatar size="w-[60px] h-[60px]" />
+          <Avatar size="w-[60px] h-[60px]" src={item.profile} />
           <div className="flex flex-col">
-            <span className="h4 font-bold text-gray-900">{item.name}</span>
+            <span className="h4 font-bold text-gray-900">{item.nickName}</span>
             <div className="space-x-2 body-1 font-medium text-gray-500">
               <span>{item.department}</span>
-              <span>{item.date}</span>
+              <span>{item.createDate}</span>
             </div>
           </div>
         </div>
@@ -32,10 +33,18 @@ export default function ClubPictureItem({ item, readonly }: Props) {
           </button>
         )}
       </div>
-      <p className="h3 font-medium text-gray-900">{item.content}</p>
-      <div className="flex gap-4 flex-nowrap w-[75vw] pb-4 overflow-x-scroll">
-        {Array.from({ length: 8 }).map((picture, i) => (
-          <div key={i} className="min-w-96 min-h-96 bg-gray-300" />
+      {/* <p className="h3 font-medium text-gray-900">{item.}</p> */}
+      <div className="flex gap-4 flex-nowrap w-[75vw] pb-4 overflow-x-auto scrollbar-custom">
+        {item.activityImages.map((picture, i) => (
+          <div key={i} className="relative w-80 h-80 bg-gray-300">
+            <Image
+              src={picture}
+              alt={`사진 ${i}`}
+              fill
+              sizes="30vw"
+              objectFit="cover"
+            />
+          </div>
         ))}
       </div>
     </div>
