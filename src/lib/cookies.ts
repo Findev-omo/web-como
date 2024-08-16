0267cb080e9bf7ec18a860db705c6272a5c2bcec
Hailey Kim <haileydeveloper@gmail.com>
2024-08-16 22:01:02 +0900

    feat: make cookies functions all async

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -2,7 +2,7 @@
 
 import { cookies } from "next/headers";
 
-export const saveAccessToken = (accessToken: string) => {
+export const saveAccessToken = async (accessToken: string) => {
   cookies().set("accessToken", accessToken);
 };
 
@@ -10,11 +10,11 @@ export const getAccessToken = async () => {
   return cookies().get("accessToken")?.value;
 };
 
-export const deleteAccessToken = () => {
+export const deleteAccessToken = async () => {
   cookies().delete("accessToken");
 };
 
-export const saveRefreshToken = (refreshToken: string) => {
+export const saveRefreshToken = async (refreshToken: string) => {
   cookies().set("refreshToken", refreshToken);
 };
 
@@ -22,18 +22,26 @@ export const getRefreshToken = async () => {
   return cookies().get("refreshToken")?.value;
 };
 
-export const deleteRefreshToken = () => {
+export const deleteRefreshToken = async () => {
   cookies().delete("refreshToken");
 };
 
-export const saveDashboardType = (type: string) => {
+export const saveDashboardType = async (type: string) => {
   cookies().set("type", type);
 };
 
-export const saveClubId = (clubId: string) => {
+export const deleteDashboardType = async () => {
+  cookies().delete("type");
+};
+
+export const saveClubId = async (clubId: string) => {
   cookies().set("clubId", clubId);
 };
 
 export const getClubId = async () => {
   return cookies().get("clubId")?.value;
+};
+
+export const deleteClubId = async () => {
+  cookies().delete("clubId");
 };
