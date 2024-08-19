5b9362dba82d992e4c795cc1545c810fbf1615a5
Hailey Kim <haileydeveloper@gmail.com>
2024-08-19 20:37:09 +0900

    feat: update ExpenseClubTab

--- src/components/dashboard/company/club/templates/ClubExpenseClubTabView.tsx
+++ src/components/dashboard/company/club/templates/ClubExpenseClubTabView.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import DropdownSearch from "@/components/common/DropdownSearch";
+
 export default function ClubExpenseClubTabView() {
-  return <></>;
+  const { push } = useRouter();
+
+  return (
+    <div className="space-y-6 p-8 rounded-xl bg-gray-0">
+      <h2 className="font-semibold text-gray-900">{"동호회 검색"}</h2>
+      <DropdownSearch
+        baseStyle="max-w-[500px]"
+        handleSelect={(selected) => push(`./expense/club/${selected}/expense`)}
+      />
+    </div>
+  );
 }
