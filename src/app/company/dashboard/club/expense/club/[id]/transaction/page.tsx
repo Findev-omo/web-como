516b0bcbae05fb59495e4b23b767ff211b2d01e3
Hailey Kim <haileydeveloper@gmail.com>
2024-08-19 21:35:39 +0900

    feat: create ClubTransactionOverview and update club transaction page

--- src/app/company/dashboard/club/expense/club/[id]/transaction/page.tsx
+++ src/app/company/dashboard/club/expense/club/[id]/transaction/page.tsx
@@ -1,3 +1,13 @@
+import BackButton from "@/components/dashboard/common/BackButton";
+import ClubTitle from "@/components/dashboard/shared/molecules/ClubTitle";
+import ClubTransactionOverview from "@/components/dashboard/company/club/molecules/ClubTransactionOverview";
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <BackButton />
+      <ClubTitle />
+      <ClubTransactionOverview />
+    </>
+  );
 }
