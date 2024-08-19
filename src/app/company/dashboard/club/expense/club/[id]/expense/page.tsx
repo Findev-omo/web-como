9cf881e5d632c12eddebdf255194130736468419
Hailey Kim <haileydeveloper@gmail.com>
2024-08-19 21:35:03 +0900

    feat: create ClubExpenseOverview and update club expense page

--- src/app/company/dashboard/club/expense/club/[id]/expense/page.tsx
+++ src/app/company/dashboard/club/expense/club/[id]/expense/page.tsx
@@ -1,3 +1,13 @@
+import BackButton from "@/components/dashboard/common/BackButton";
+import ClubTitle from "@/components/dashboard/shared/molecules/ClubTitle";
+import ClubExpenseOverview from "@/components/dashboard/company/club/molecules/ClubExpenseOverview";
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <BackButton />
+      <ClubTitle />
+      <ClubExpenseOverview />
+    </>
+  );
 }
