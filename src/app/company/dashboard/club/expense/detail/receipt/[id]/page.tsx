d86c76cac11314984efa44623b90a9131d51fac9
Hailey Kim <haileydeveloper@gmail.com>
2024-08-19 17:53:51 +0900

    feat: move receipt page under detail and update contents

---
+++ src/app/company/dashboard/club/expense/detail/receipt/[id]/page.tsx
@@ -0,0 +1,22 @@
+import BackButton from "@/components/dashboard/common/BackButton";
+import DocUtilButtons from "@/components/dashboard/common/DocUtil";
+import PDFViewer from "@/components/dashboard/club/common/PDFViewer";
+
+interface Props {
+  params: { id: string };
+}
+
+export default function ExpenseReceiptDetailPage({ params }: Props) {
+  return (
+    <>
+      <BackButton />
+      <div className="p-8 rounded-xl bg-gray-0">
+        <div className="flex items-center justify-between mb-4">
+          <h2 className="h3 font-semibold text-gray-900">{"수령증"}</h2>
+            <DocUtilButtons />
+        </div>
+        <PDFViewer file="../../../../../../sample.pdf" />
+      </div>
+    </>
+  );
+}
