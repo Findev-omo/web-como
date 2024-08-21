9cd72e489a3f5cc14b6060ea97932d1f826ece9f
Hailey Kim <haileydeveloper@gmail.com>
2024-08-21 16:45:39 +0900

    feat: create /announcement/document and /document/[id] pages

---
+++ src/app/club/dashboard/announcement/document/[id]/page.tsx
@@ -0,0 +1,11 @@
+import BackButton from "@/components/dashboard/common/BackButton";
+import DocumentDetail from "@/components/dashboard/company/employee/organisms/DocumentDetail";
+
+export default function Page() {
+  return (
+    <>
+      <BackButton />
+	  <DocumentDetail />
+    </>
+  );
+}
--- src/app/club/dashboard/announcement/document/page.tsx
+++ src/app/club/dashboard/announcement/document/page.tsx
@@ -1,3 +1,9 @@
+import DocumentView from "@/components/dashboard/company/employee/templates/DocumentView";
+
 export default function DocumentPage() {
-  return <></>;
+  return (
+    <>
+      <DocumentView />
+    </>
+  );
 }
