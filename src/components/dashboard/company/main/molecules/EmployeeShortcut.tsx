a3f1fa0c7c18ee8d42d037f83ade5ee11065c9ed
Hailey Kim <haileydeveloper@gmail.com>
2024-08-21 19:06:19 +0900

    feat: update EmployeeShortcut for use in employee page

--- src/components/dashboard/company/main/molecules/EmployeeShortcut.tsx
+++ src/components/dashboard/company/main/molecules/EmployeeShortcut.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import Link from "next/link";
-import { COMPANY_DASHBOARD_ENDPOINT } from "@/lib/constants";
+import { usePathname } from "next/navigation";
 import { ChevronRight } from "@/assets/icons/chevron";
 
 export default function EmployeeShortcut() {
+  const pathname = usePathname();
+
+  const Container = ({ children }: { children: React.ReactNode }) => {
+    const style =
+      "flex-1 space-y-4 max-w-[390px] p-8 rounded-xl bg-gray-0 select-none";
+
+    if (pathname.endsWith("dashboard")) {
+      return (
+        <Link href={`./employee`} className={style}>
+          {children}
+        </Link>
+      );
+    } else {
+      <div className={style}>{children}</div>;
+    }
+  };
+
   return (
-    <Link
-      href={`${COMPANY_DASHBOARD_ENDPOINT}/employee`}
-      className="flex-1 space-y-4 max-w-[390px] p-8 rounded-xl bg-gray-0 select-none"
-    >
+    <Container>
       <div className="h4 font-medium text-gray-700 truncate">
         {"전체 임직원 수"}
       </div>
@@ -15,8 +31,10 @@ export default function EmployeeShortcut() {
         <div className="h1 font-extrabold text-gray-900 truncate">
           {"1234명"}
         </div>
-        <ChevronRight className="w-8 h-8 text-gray-600" />
+        {pathname.endsWith("dashboard") && (
+          <ChevronRight className="w-8 h-8 text-gray-600" />
+        )}
       </div>
-    </Link>
+    </Container>
   );
 }
