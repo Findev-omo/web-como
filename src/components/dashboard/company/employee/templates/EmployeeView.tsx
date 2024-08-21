beb0eee9bead437ce457a27d1c397cb2413ab630
Hailey Kim <haileydeveloper@gmail.com>
2024-08-21 19:08:28 +0900

    feat: create EmployeeView

---
+++ src/components/dashboard/company/employee/templates/EmployeeView.tsx
@@ -0,0 +1,11 @@
+import EmployeeSearch from "@/components/dashboard/company/employee/molecules/EmployeeSearch";
+import EmployeeList from "@/components/dashboard/company/employee/organisms/EmployeeList";
+
+export default function EmployeeView() {
+  return (
+    <>
+      <EmployeeSearch />
+	  <EmployeeList />
+    </>
+  );
+}
