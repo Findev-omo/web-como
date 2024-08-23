ce3afa64f6ae63b5efeb5a1db71fd893a73a11fe
Hailey Kim <haileydeveloper@gmail.com>
2024-08-23 13:53:18 +0900

    feat: update ClubQnaQuestionContent

--- src/components/dashboard/club/manage/molecules/ClubQnaQuestionContent.tsx
+++ src/components/dashboard/club/manage/molecules/ClubQnaQuestionContent.tsx
@@ -1,31 +1,37 @@
+import type { QnaQuestionDetailData } from "@/api/types/club/question/detail";
+import { formatDate } from "@/lib/utils";
 import Avatar from "@/components/common/Avatar";
 
-export default function ClubQnaQuestionContent() {
-  const content = `식사는 따로 지참하나요?
-  그리고 끝나고 회식 있는지 궁금합니다 ㅎㅎ
-  운영에 도움 주셔서 감사합니다!`;
+interface Props {
+  data: QnaQuestionDetailData;
+}
 
+export default function ClubQnaQuestionContent({ data }: Props) {
   return (
     <div className="space-y-8">
       <div className="flex items-start justify-between">
         <div className="flex items-center gap-3">
-          <Avatar size="w-[52px] h-[52px]" />
-          <div className="flex flex-col">
-            <span className="h4 font-bold text-gray-900">{"바다사랑"}</span>
-            <span className="body-1 font-medium text-gray-500">
-              {"미래사업전략"}
-            </span>
+          <Avatar size="w-[52px] h-[52px]" src={data.questionerProfile} />
+          <div className="space-y-1">
+            <div className="h4 font-bold text-gray-900">
+              {data.questionerName}
+            </div>
+            <div className="body-1 font-medium text-gray-500">
+              {data.questionerDepartment}
+            </div>
           </div>
         </div>
         <div className="flex items-center gap-2">
-          <span className="body-1 font-medium text-gray-500">{"비공개"}</span>
+          <div className="body-1 font-medium text-gray-500">
+            {data.isSecret ? "비공개" : "공개"}
+          </div>
           <span className="h-4 border-l border-gray-500" />
-          <span className="body-1 font-medium text-gray-500">
-            {"2024.04.16"}
-          </span>
+          <div className="body-1 font-medium text-gray-500">
+            {formatDate(new Date(data.createdDate))}
+          </div>
         </div>
       </div>
-      <p className="h3 font-medium text-gray-900">{content}</p>
+      <p className="h3 font-medium text-gray-900">{data.content}</p>
     </div>
   );
 }
