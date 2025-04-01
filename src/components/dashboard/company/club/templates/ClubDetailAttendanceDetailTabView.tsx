943c5a5cfd391fb56d98656554d6fb6c3706a7cb
2sojeong <dlgur200123@naver.com>
2025-04-01 15:33:19 +0900

    feat:fix bug

--- src/components/dashboard/company/club/templates/ClubDetailAttendanceDetailTabView.tsx
+++ src/components/dashboard/company/club/templates/ClubDetailAttendanceDetailTabView.tsx
@@ -1,10 +1,15 @@
 import EmployeeInfo from "@/components/dashboard/company/club/molecules/EmployeeInfo";
 import AttendanceList from "@/components/dashboard/company/club/organisms/AttendanceList";
+import { useParams } from "next/navigation";
 
 export default function ClubDetailAttendanceDetailTabView() {
+  const params = useParams();
+  const memberId = params.id as string;
   return (
     <>
-      <EmployeeInfo />
+      <EmployeeInfo
+        memberId={memberId}
+      />
       <AttendanceList />
     </>
   );
