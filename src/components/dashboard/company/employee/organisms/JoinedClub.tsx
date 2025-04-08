23e90844195ce14ce93cccc7b23b90af0232a7da
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2025-04-08 20:38:47 +0900

    refactor: JoinedClub 컴포넌트의 리스트 출력 주석 처리

--- src/components/dashboard/company/employee/organisms/JoinedClub.tsx
+++ src/components/dashboard/company/employee/organisms/JoinedClub.tsx
@@ -4,11 +4,12 @@ export default function JoinedClub() {
   return (
     <div className="space-y-6 p-8 rounded-xl bg-gray-0">
       <h3 className="h2 font-bold text-gray-900">{"가입한 동호회"}</h3>
-      <ul className="flex gap-3">
+      {/* <ul className="flex gap-3">
         {[1, 2, 3].map((club) => (
           <ClubCard key={club} />
         ))}
-      </ul>
+      </ul> */}
+      <p className="text-lg text-gray-500">가입 중인 동호회가 없습니다.</p>    
     </div>
   );
 }
