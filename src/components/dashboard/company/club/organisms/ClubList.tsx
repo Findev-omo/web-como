5f7ab8c2288744a9e9e8c997ef8a0267f128ae94
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2025-04-13 21:54:08 +0900

    fix: 사내 동호회 테이블 데이터 배치 로직 수정

--- src/components/dashboard/company/club/organisms/ClubList.tsx
+++ src/components/dashboard/company/club/organisms/ClubList.tsx
@@ -6,6 +6,9 @@ import DateFilter, {
 } from "@/components/dashboard/common/DateFilter";
 import Pagination from "@/components/dashboard/common/Pagination";
 import ClubTable from "@/components/dashboard/company/club/molecules/ClubTable";
+import { startOfToday } from "date-fns";
+import { getData } from "@/api/action";
+import { useEffect } from "react";
 
 interface Props {
   currentSearchTerm: string;
@@ -13,11 +16,37 @@ interface Props {
 }
 
 export default function ClubList(props: Props) {
+  const [clubs, setClubs] = useState([]);
   const [currentDateRange, setCurrentDateRange] = useState<DateRange>({
-    startDate: undefined,
-    endDate: undefined,
+    startDate: startOfToday(),
+    endDate: startOfToday(),
   });
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(1);
+
+  const formatDateToString = (date: Date | undefined) => {
+    if (!date) return '';
+    const koreaDate = new Date(date.getTime() + (9 * 60 * 60 * 1000));
+    return koreaDate.toISOString().split('T')[0];
+  };
+
+  const loadClubs = async () => {
+    try {
+      const response = await getData(`v1/manager/club/manage-list?page=${currentPage}&startDate=${formatDateToString(currentDateRange.startDate)}&endDate=${formatDateToString(currentDateRange.endDate)}`, true);
+      console.log(response.data)
+      if (response.resultCode === 'OK') {
+        console.log("Club Data:", response.data); // 데이터 확인
+        setClubs(response.data.manageClubList);
+        setMaxPage(response.data.maxPage);
+      }
+    } catch (error) {
+      console.error("사내 동호회 목록 로딩 오류:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadClubs();
+  }, [currentPage, currentDateRange]);
 
   const handleDateRangeChange = (dateRange: DateRange) => {
     setCurrentDateRange(dateRange);
@@ -36,12 +65,16 @@ export default function ClubList(props: Props) {
         handleDateRangeChange={handleDateRangeChange}
       />
       <div className="space-y-10">
-        <ClubTable />
-        <Pagination
-          currentPage={currentPage}
-          maxPage={8}
-          handlePageChange={handlePageChange}
+        <ClubTable 
+          clubs={clubs}
         />
+        {clubs && clubs.length > 0 && (
+          <Pagination
+            currentPage={currentPage}
+            maxPage={maxPage}
+            handlePageChange={handlePageChange}
+          />
+        )}
       </div>
     </div>
   );
