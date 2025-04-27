c8fd5effa69611ca490f334df10f94150a545b12
minki3 <86992006+minki3@users.noreply.github.com>
2025-04-27 16:34:53 +0900

    refactor:필요없는 데이터 제거

--- src/components/dashboard/company/club/templates/ClubDetailPictureTabView.tsx
+++ src/components/dashboard/company/club/templates/ClubDetailPictureTabView.tsx
@@ -5,34 +5,6 @@ import ClubPictureItem from "@/components/dashboard/shared/organisms/ClubPicture
 import { useQuery } from "@tanstack/react-query";
 import { usePathname } from "next/navigation";
 
-export interface ClubPicture {
-  id: number;
-  name: string;
-  department: string;
-  date: string;
-  content: string;
-  pictureList: string[];
-}
-
-const pictureList: ClubPicture[] = [
-  {
-    id: 1,
-    name: "닉네임",
-    department: "부서",
-    date: "2024.07.06",
-    content: "최고의 스킨 스쿠버였습니다~ 다음번에는 다른분들과 함께해요",
-    pictureList: [],
-  },
-  {
-    id: 2,
-    name: "닉네임",
-    department: "부서",
-    date: "2024.07.06",
-    content: "최고의 스킨 스쿠버였습니다~ 다음번에는 다른분들과 함께해요",
-    pictureList: [],
-  },
-];
-
 export default function ClubDetailPictureTabView() {
   const pathname = usePathname();
   const clubId = pathname.split("/").pop();
