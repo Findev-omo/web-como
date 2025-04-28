48de025598c889d651a85b6d407431bf8319ae2c
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2025-04-29 02:19:22 +0900

    fix: 공지사항 관리 페이지의 검색바(SearchBar) 렌더링 임시 비활성화

--- src/components/dashboard/club/clubAnnouncement/molecules/AnnouncementSearch.tsx
+++ src/components/dashboard/club/clubAnnouncement/molecules/AnnouncementSearch.tsx
@@ -11,12 +11,12 @@ export default function AnnouncementSearch() {
   return (
     <div className="space-y-6 p-8 rounded-xl bg-gray-0">
       <h2 className="font-semibold text-gray-900">{"공지사항 관리"}</h2>
-      <SearchBar
+      {/* <SearchBar
         placeholder="검색어를 입력하세요."
         currentValue={currentSearchTerm}
         handleChange={(e) => setCurrentSearchTerm(e.target.value)}
         handleSubmit={handleSearch}
-      />
+      /> */}
     </div>
   );
 }
