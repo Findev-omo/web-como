1d99a595dcef76ee6f25806453a9c5700363d13b
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2025-05-01 02:51:27 +0900

    feat: 일정 관리 페이지에 기능 적용일 안내 문구 추가

--- src/components/dashboard/club/schedule/molecues/ScheduleTitle.tsx
+++ src/components/dashboard/club/schedule/molecues/ScheduleTitle.tsx
@@ -2,6 +2,9 @@ const ScheduleTitle = async () => {
   return (
     <div className="space-y-2 p-8 rounded-xl bg-gray-0">
       <h2 className="h1 font-bold text-black">{"동호회 일정 관리"}</h2>
+      <p className="body-2 text-gray-500 font-bold text-lg">
+        해당 기능의 알림이 05.01 (목) 17시 부터 적용되오니 이용에 참고 부탁드립니다.
+</p>
     </div>
   );
 };
