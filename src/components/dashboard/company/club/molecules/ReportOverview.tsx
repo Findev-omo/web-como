93721c036e6911f472b262227bcec50aeda8674f
hobeom <hobeom2049@gmail.com>
2025-05-09 14:06:00 +0900

    feat: 보고서 현황 조회

--- src/components/dashboard/company/club/molecules/ReportOverview.tsx
+++ src/components/dashboard/company/club/molecules/ReportOverview.tsx
@@ -1,20 +1,39 @@
+"use client";
+import { getSummary } from "@/api/actions/company/report/getSummary";
+import { useEffect, useState } from "react";
+
 export default function ReportOverview() {
+  const [pendingCount, setPendingCount] = useState(0);
+  const [approvedCount, setApprovedCount] = useState(0);
+  const [rejectedCount, setRejectedCount] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { pendingCount, approvedCount, rejectedCount } = await getSummary();
+      setPendingCount(pendingCount);
+      setApprovedCount(approvedCount);
+      setRejectedCount(rejectedCount);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="flex flex-col gap-6 h-fit p-8 rounded-xl bg-gray-800 select-none">
-      <h2 className="h1 font-bold text-gray-0">{"보고서 관리"}</h2>
+      <h2 className="h1 font-bold text-gray-0">{"활동비 신청 내역"}</h2>
       <div className="flex gap-8 truncate">
         <div className="flex-1 flex flex-col gap-4 py-3 px-2">
-          <h4 className="font-medium text-gray-400">{"미확인 보고서"}</h4>
+          <h4 className="font-medium text-gray-400">{"승인대기"}</h4>
           <span className="h1 font-extrabold text-brand-orange">
-            {`${2}건`}
+            {`${pendingCount}건`}
           </span>
         </div>
         <span className="h-[104px] border-l border-gray-700" />
         <div className="flex-1 flex flex-col gap-4 py-3 px-2">
-          <h4 className="font-medium text-gray-400">
-            {"작성대기 중인 보고서"}
-          </h4>
-          <span className="h1 font-extrabold text-gray-0">{`${6}건`}</span>
+          <h4 className="font-medium text-gray-400">{"승인완료"}</h4>
+          <span className="h1 font-extrabold text-gray-0">{`${approvedCount}건`}</span>
+        </div>
+        <span className="h-[104px] border-l border-gray-700" />
+        <div className="flex-1 flex flex-col gap-4 py-3 px-2">
+          <h4 className="font-medium text-gray-400">{"반려완료"}</h4>
+          <span className="h1 font-extrabold text-gray-0">{`${rejectedCount}건`}</span>
         </div>
       </div>
     </div>
