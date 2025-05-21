01a605792d7b2100e260503e3a9b3605a8dff036
hobeom <hobeom2049@gmail.com>
2025-05-21 10:12:15 +0900

    layout: 사이드바 메뉴수정

--- src/components/dashboard/company/common/SideBar.tsx
+++ src/components/dashboard/company/common/SideBar.tsx
@@ -15,6 +15,7 @@ const companyDashboardMenus = [
   "announcement",
   "community",
   "report",
+  "expense",
 ] as const;
 
 type CompanyDashboardMenu = (typeof companyDashboardMenus)[number];
@@ -68,8 +69,14 @@ const menuList: MenuItem[] = [
         link: "/club",
         routes: ["/club", "/club/detail"],
       },
+    ],
+  },
+  {
+    name: "활동지원비 관리",
+    key: "expense",
+    subMenuList: [
       {
-        name: "활동지원비 신청 관리",
+        name: "활동지원비 관리",
         link: "/club/expense",
         routes: ["/club/expense"],
       },
