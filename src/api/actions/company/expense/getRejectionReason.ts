25f6e5374155910cc9830bddf9fb525ebe7f2586
kirnjiyun <138554290+kirnjiyun@users.noreply.github.com>
2025-08-19 13:51:33 +0900

    hotfix: 반려사유

--- src/api/actions/company/expense/getRejectionReason.ts
+++ src/api/actions/company/expense/getRejectionReason.ts
@@ -1,19 +1,9 @@
-import { getAccessToken } from "@/lib/cookies";
-import { buildAbsoluteUrl } from "@/lib/client-utils";
+import { getData } from "@/api/action";
 
 export const getRejectionReason = async (expenseId: string) => {
-  const token = await getAccessToken();
-  if (!token) throw new Error("토큰 정보가 없습니다.");
-
-  const endpoint = `api/server/v1/manager/activity-expenses/${expenseId}/rejection-reason`;
-  const url = buildAbsoluteUrl(endpoint);
-
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      accept: "application/json",
-    },
-  });
-  return response.json();
+  const response = await getData(
+    `v1/manager/activity-expenses/${expenseId}/rejection-reason`,
+    false
+  );
+  return response.data;
 };
