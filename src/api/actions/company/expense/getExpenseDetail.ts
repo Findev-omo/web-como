7d99b5cf73f84c4910a556e1b3c36e4044756d2d
kirnjiyun <138554290+kirnjiyun@users.noreply.github.com>
2025-08-19 13:38:56 +0900

    hotfix: 활동지원비, 보고서 관리

--- src/api/actions/company/expense/getExpenseDetail.ts
+++ src/api/actions/company/expense/getExpenseDetail.ts
@@ -1,19 +1,6 @@
-import { getAccessToken } from "@/lib/cookies";
-import { buildAbsoluteUrl } from "@/lib/client-utils";
+import { getData } from "@/api/action";
 
 export const getExpenseDetail = async (expenseId: string) => {
-  const token = await getAccessToken();
-  if (!token) throw new Error("토큰 정보가 없습니다.");
-
-  const endpoint = `api/server/v1/manager/activity-expenses/${expenseId}`;
-  const url = buildAbsoluteUrl(endpoint);
-
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      accept: "application/json",
-    },
-  });
-  return response.json();
+  const response = await getData(`v1/manager/activity-expenses/${expenseId}`);
+  return response;
 };
